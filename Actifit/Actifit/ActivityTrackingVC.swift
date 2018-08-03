5f6f2ad0524ee602ea037e0ae49036900fd5656c
Hitender kumar <hitender.hk@gmail.com>
2018-08-03 14:39:14 +0530

    Project setup

---
+++ Actifit/Actifit/ActivityTrackingVC.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTrackingVC.swift
+//  Actifit
+//
+//  Created by Hitender kumar on 03/08/18.
+//
+
+import UIKit
+
+class ActivityTrackingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

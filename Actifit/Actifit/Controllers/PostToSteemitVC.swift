1a4b2b01c279ecf415bd27a9c6fce9dd109d135c
hitender kumar <hitender.hk@gmail.com>
2018-08-15 00:45:09 +0530

    Post to steemit UI part complete

--- Actifit/Actifit/Controllers/PostToSteemitVC.swift
+++ Actifit/Actifit/Controllers/PostToSteemitVC.swift
@@ -27,17 +27,35 @@ class PostToSteemitVC: UIViewController {
     @IBOutlet weak var postContentTextView : AFTextView!
     @IBOutlet weak var postContentTextViewHeightConstraint : NSLayoutConstraint!
     @IBOutlet weak var postToSteemitBtn : UIButton!
+    
+    @IBOutlet weak var postTitleTextViewLineLabel : UIView!
+    @IBOutlet weak var postTagsTextViewLineLabel  : UIView!
+    @IBOutlet weak var postContentTextViewLineLabel  : UIView!
+
 
     //MARK: VIEW LIFE CYCLE
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.postTitleTextView.delegate = self
+        self.postTagsTextView.delegate = self
+        self.postContentTextView.delegate = self
+
         self.backBtn.setImage(#imageLiteral(resourceName: "back_black").withRenderingMode(.alwaysTemplate), for: .normal)
         self.backBtn.tintColor = UIColor.white
         self.postContentTextView.heightConstraint = self.postContentTextViewHeightConstraint
         self.postTitleTextView.heightConstraint = self.postTitleTextViewHeightConstraint
         self.postTagsTextView.heightConstraint = self.postTagsTextViewHeightConstraint
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       
     }
     
     //MARK: INTERFACE BUILDER ACTIONS
@@ -52,5 +70,34 @@ class PostToSteemitVC: UIViewController {
     
     @IBAction func postToSteemitBtnAction(_ sender : UIButton) {
         
+    }
+}
+
+extension PostToSteemitVC : UITextViewDelegate{
+    
+    //MARK: UITextViewDelegate
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if let afTextView = (textView as? AFTextView) {
+            if afTextView.isEqual(self.postTitleTextView) {
+                self.postTitleTextViewLineLabel.backgroundColor = UIColor.red
+            } else if afTextView.isEqual(self.postTagsTextView) {
+                self.postTagsTextViewLineLabel.backgroundColor = UIColor.red
+            } else if afTextView.isEqual(self.postContentTextView) {
+                self.postContentTextViewLineLabel.backgroundColor = UIColor.red
+            }
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if let afTextView = (textView as? AFTextView) {
+            if afTextView.isEqual(self.postTitleTextView) {
+                self.postTitleTextViewLineLabel.backgroundColor = UIColor.lightGray
+            } else if afTextView.isEqual(self.postTagsTextView) {
+                self.postTagsTextViewLineLabel.backgroundColor = UIColor.lightGray
+            } else if afTextView.isEqual(self.postContentTextView) {
+                self.postContentTextViewLineLabel.backgroundColor = UIColor.lightGray
+            }
+        }
     }
 }
--- Actifit/Actifit/Inline Helpers/AFTextView.swift
+++ Actifit/Actifit/Inline Helpers/AFTextView.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class AFTextView: UITextView {
     
-    lazy var lineLabel : UILabel = {
-        return UILabel()
-    }()
-    
     //MARK: View Life Cycle
     
     override func layoutSubviews() {
@@ -25,10 +21,7 @@ class AFTextView: UITextView {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         self.clipsToBounds = false
-        self.registerTextViewBeginEditingNotification()
-        self.registerTextViewEndEditingNotification()
         self.registerTextViewTextChangeNotification()
-        self.setUpLineLabel()
     }
     
     deinit{
@@ -37,51 +30,12 @@ class AFTextView: UITextView {
     
     //MARK: Private Helpers
     
-    private func setUpLineLabel() {
-        self.lineLabel.backgroundColor = UIColor.lightGray
-        self.addSubview(self.lineLabel)
-
-        self.lineLabel.prepareForNewConstraints(block: { (view) in
-            self.lineLabel.setBottomSpaceFromSuperView(bottomSpace: 35)
-            self.lineLabel.setLeadingSpaceFromSuperView(leadingSpace: 0)
-            self.lineLabel.setTrailingSpaceFromSuperView(trailingSpace: 0)
-            self.lineLabel.pinHeightConstraint(constant: 1)
-        })
-    }
-    
-    
-    private func highlightLineLabel()  {
-        self.lineLabel.backgroundColor = UIColor.red
-    }
-    
-    private func unHighlightLineLabel()  {
-        self.lineLabel.backgroundColor = UIColor.lightGray
-    }
-    
-    @objc private func txtViewDidBeignEditing(){
-        self.highlightLineLabel()
-    }
-    
-    @objc private func txtViewDidEndEditing(){
-        self.unHighlightLineLabel()
-    }
-    
     @objc private func txtViewTextDidChange(){
-        let attrbutedStr = self.text.attributedString(font: UIFont.systemFont(ofSize: 15))
-        let newHeight = attrbutedStr.heightFor(boundingWidth: ScreenWidth - 20)
-        if newHeight < 30 { return }
-        self.heightConstraint?.constant = newHeight
-        self.superview?.layoutIfNeeded()
-        self.sizeToFit()
-        self.isScrollEnabled = false
-    }
-    
-    private func registerTextViewBeginEditingNotification(){
-        NotificationCenter.default.addObserver(self, selector:#selector(txtViewDidBeignEditing), name:NSNotification.Name.UITextViewTextDidBeginEditing, object: self)
-    }
-    
-    private func registerTextViewEndEditingNotification(){
-        NotificationCenter.default.addObserver(self, selector:#selector(txtViewDidEndEditing), name:NSNotification.Name.UITextViewTextDidEndEditing, object: self)
+        //http://iosanchor.com/dynamically-change-parent-uiviews-height-textview-height-changes/
+        let fixedWidth = self.frame.size.width
+        let newSize = self.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        self.heightConstraint?.constant = newSize.height
+        self.layoutIfNeeded()
     }
     
     private func registerTextViewTextChangeNotification(){
